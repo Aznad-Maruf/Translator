1d1e26b9334e8dcc34e31c00b46fac615ff04ab7
Aznad-Maruf <aznadmaruf@gmail.com>
2024-03-05 11:46:39 +0600

    Initial commit

---
+++ src/test/java/hood/robin/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package hood.robin.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
